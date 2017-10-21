c64e26b68532385bbf3d8f1de7ade527da01b576
ben <benhgift@gmail.com>
2017-10-21 11:22:45 -0700

    'multiplayer'

--- n.py
+++ n.py
@@ -3,12 +3,14 @@ import os
 import time
 import subprocess
 import fire
+import psutil
 from box import Box
 from pyautogui import press
 from pygame import JOYAXISMOTION, JOYHATMOTION, JOYBUTTONDOWN, JOYBUTTONUP
 from random import sample
 from glob import glob
 from collections import namedtuple
+from os.path import basename
 
 
 def handle_buttons(btns):
@@ -19,7 +21,7 @@ def handle_buttons(btns):
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit ()
-            import sys; sys.exit() #meh
+            import sys; sys.exit() #meh, it's not exiting nicely, screw it
             done=True
         if event.type in [JOYAXISMOTION, JOYHATMOTION, JOYBUTTONDOWN, JOYBUTTONUP]:
             if event.type == JOYBUTTONDOWN and event.button < 8:
@@ -29,11 +31,15 @@ def handle_buttons(btns):
                 btn = val_btn_map[event.button]
                 btns[btn] = 0
             btns.something_happened = True
+    init_joysticks()
+    return btns, done
+
+
+def init_joysticks():
     joystick_count = pygame.joystick.get_count()
     for i in range(joystick_count):
         joystick = pygame.joystick.Joystick(i)
         joystick.init()
-    return btns, done
 
 
 def turn_it_up_or_down(happened, volume, last_time, now):
@@ -50,11 +56,19 @@ def turn_it_up_or_down(happened, volume, last_time, now):
 
 
 def run_gba_emulator(game):
-    return subprocess.Popen(r'C:\Users\corn8\Desktop\mGBA-0.6.0-2017-07-16-win32\mGBA.exe ' + '"' + game + '"' + ' -f')
+    return subprocess.Popen(r'C:\Users\corn8\Documents\mGBA-0.6.0-2017-07-16-win32\mGBA.exe ' + '"' + game + '"' + ' -f')
 
 
 def run_snes_emulator(game):
-    return subprocess.Popen(r'C:\Users\corn8\Desktop\snes9x\snes9x-x64.exe ' + '"' + game + '"' + ' -fullscreen')
+    return subprocess.Popen(r'C:\Users\corn8\Documents\snes9x\snes9x-x64.exe ' + '"' + game + '"' + ' -fullscreen')
+
+
+def run_steam_exe_game(game):
+    subprocess.Popen(game)
+    time.sleep(1)
+    for x in psutil.process_iter():
+        if basename(game) in x.name():
+            return x
 
 
 def shuffle_games_if_last_one(games, game_idx):
@@ -89,9 +103,13 @@ def swap_games(games, game_idx, emulator_process):
     game_idx += 1
     print('Now playing: ' + game.game_path)
     emulator_process = game.emulator_function(game.game_path)
-    time.sleep(1.5)
+    time.sleep(1)
     old_emulator_process.terminate()
     return games, game_idx, emulator_process
+
+
+def pid_is_running(pid):        
+    psutil.pid_exists(pid)
 
 
 def main(seed_game=None):
@@ -107,15 +125,16 @@ def main(seed_game=None):
     volume = 'down'
     last_time = time.time()
 
-    gba_files = glob(os.path.join(r'C:\Users\corn8\Desktop\roms\gba_roms', '*.zip'))
-    gbc_files = glob(os.path.join(r'C:\Users\corn8\Desktop\roms\gbc_roms', '*.zip'))
-    snes_files = glob(os.path.join(r'C:\Users\corn8\Desktop\roms\snes_roms', '*.zip'))
+    gba_files = glob(os.path.join(r'C:\Users\corn8\Documents\roms\gba_roms', '*.zip'))
+    gbc_files = glob(os.path.join(r'C:\Users\corn8\Documents\roms\gbc_roms', '*.zip'))
+    snes_files = glob(os.path.join(r'C:\Users\corn8\Documents\roms\snes_roms', '*.zip'))
     games = [Game(gba_file, run_gba_emulator) for gba_file in gba_files]
     games += [Game(gbc_file, run_gba_emulator) for gbc_file in gbc_files]
     games += [Game(snes_file, run_snes_emulator) for snes_file in snes_files]
     games = sample(games, len(games))
+    multiplayer_games = [Game(r"C:\Program Files (x86)\Steam\steamapps\common\TowerFall\TowerFall.exe", run_steam_exe_game)]
     game_idx = 1
-    emulator_process = start_the_first_game(games, seed_game)
+    emulator_process = start_the_first_game(multiplayer_games, seed_game)
 
     print('starting... press crtl c to quit')
     while done==False:
@@ -127,9 +146,17 @@ def main(seed_game=None):
         if not buttons.something_happened and time_to_swap_games(now, last_time):
             games, game_idx, emulator_process = swap_games(games, game_idx, emulator_process)
             last_time = now
-        if all(buttons.values()):
+        if all([buttons[x] for x in ['something_happened', 'a', 'b', 'l', 'r', 'start']]):
             print('---forcing game override because the last game sucked')
             games, game_idx, emulator_process = swap_games(games, game_idx, emulator_process)
+            time.sleep(2)
+            for key in buttons.keys():
+                buttons[key] = 0
+                buttons.something_happened = False
+        elif all([buttons[x] for x in ['something_happened', 'a', 'b', 'l', 'r', 'select']]):
+            print('---starting multiplayer')
+            _, _, emulator_process = swap_games(multiplayer_games, 0, emulator_process)
+            time.sleep(2)
             for key in buttons.keys():
                 buttons[key] = 0
                 buttons.something_happened = False
